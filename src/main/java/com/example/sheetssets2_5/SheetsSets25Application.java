42f0f0bd3cd86ad416fe6cc60660a2bc5c738312
Heppy <lecsa515@gmail.com>
2023-05-15 22:46:37 +0300

    Start

---
+++ src/main/java/com/example/sheetssets2_5/SheetsSets25Application.java
@@ -0,0 +1,13 @@
+package com.example.sheetssets2_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsSets25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetsSets25Application.class, args);
+    }
+
+}
